831d14f07edaca4b62d2f542406953f407675356
Pao, Janos Syd Nepthali <Janos_syd_nepthali.Pao@atmel.com>
2017-02-08 09:45:24 +0800

    Xmega Cpp project

---
+++ XmegaCpp/XmegaCpp/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * XmegaCpp.cpp
+ *
+ * Created: 2/8/2017 9:26:01 AM
+ * Author : Syd Pao
+ */ 
+
+#include <avr/io.h>
+
+//void * operator new(size_t size);
+void operator delete(void * ptr);
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
